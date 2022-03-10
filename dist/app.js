523bdc8eb2f518b8de44f749f0ff741f2bfd3f2f
Hieu Minh <nmhieu04091999@gmail.com>
2022-03-10 12:16:39 +0700

    delete file

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const a = 10;
-const name = "Hieu";
-const status = false;
-const stringArr = ["a", "b", "c", "d"];
--- src/app.ts
+++
@@ -1,6 +0,0 @@
-export {}
-const a: number = 10;
-const name: string = "Hieu";
-const status: boolean = false;
-const arr: any[] = ['a',1,2];
-const stringArr: string[]= ["a", "b", "c", "d"]
